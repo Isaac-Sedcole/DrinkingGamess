a38bb098dcec8c3851858fc3f790ad87447668ba
Isaac Sedcole <isaacsedcolei@gmail.com>
2021-08-28 10:45:21 +1200

    added another dataSet and made adjustments to old one

--- data/gamesList.js
+++ data/gamesList.js
@@ -5,6 +5,7 @@ export default {
             name: "Kings Cup",
             suggestedPlayers: "4+",
             drunkMeter: 6,
+            itemsRequired: ["Pack of cards", "Large bowl or cup"],
             explanationBlurb: "its the kings cup",
             rules: {
                 ace: "snake eyes",
@@ -20,6 +21,13 @@ export default {
                 jack: "make a rule",
                 queen: "question master",
                 king: "empty your vessel into the kings cup"
+            },
+            customRules: [
+                "snake eyes", "you", "me", "whores", "gecko", "dicks", "heaven", "mate" 
+            ],
+            ruleDescription: {
+                snakeEyes: "If you look this person in the yes you must drink",
+
             }
         },
         {
@@ -27,12 +35,17 @@ export default {
             name: "Smash Cup",
             suggestedPlayers: "4+",
             drunkMeter: 4,
+            itemsRequired: ["Pack of cards", "Large bowl or cup"],
             explanationBlurb: "get yo balls in the cup boi",
             rules: {
                 inTheCup: "if you bounce the ball into the cup you make hand the ball and cup to the next person",
                 inTheCupFirstGo: "if you bounce the ball into the cup on the first go you make hand the ball and cup to any other person",
                 getDunked: "if your cup gets dunked, then you must consume before continuing to play",
                 spill: "if someone spills they must consume the drink before continuing to play"
+            },
+            customRules: [],
+            ruleDescription: {
+
             }
         },
         {
@@ -40,12 +53,17 @@ export default {
             name: "Taxi Driver",
             suggestedPlayers: "2+",
             drunkMeter: 7,
+            itemsRequired: ["Pack of cards", "Large bowl or cup"],
             explanationBlurb: "Its taxi driver",
             rules: {
                 higher: "choose if the next card flipped will be higher than the current card - if correct you move forward",
                 lower: "choose if the next card flipped will be lower than the current card - if correct you move forward",
                 same: "choose if the next card flipped will be lower than the current card - if correct you win",
                 incorrect: "if incorrect go back to the start"
+            },
+            customRules: [],
+            ruleDescription: {
+                
             }
         }
     ]
---
+++ data/houseRules.js
@@ -0,0 +1,16 @@
+export default {
+    houseRules: [
+        {
+            id: 1,
+            name: "No D word",
+            description: "if drink is said, you must consume",
+            punishment: 1
+        },
+        {
+            id: 2,
+            name: "No pointing",
+            description: "if drink is said, you must consume",
+            punishment: 1
+        }
+    ]
+}
