99a56cc16bf4ce1accd7f512307cf77057411ed6
Isaac Sedcole <isaacsedcolei@gmail.com>
2021-09-03 19:07:11 +1200

    updated styling for Games page to be more inline with general guidelines

--- components/Games.js
+++ components/Games.js
@@ -1,67 +1,80 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableHighlight, View, Button } from 'react-native'
 import { Card } from 'react-native-paper'
 import gamesList from '../data/gamesList'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 import Game from './Game'
 import HouseRules from './HouseRules'
 
 
-function Games ({ navigation }) {
+function Games({ navigation }) {
 
-    // const renderGames = () => {
-    //     for(let i in gamesList) {
-    //         console.log(i)
-    //         if(i.name == undefined) {
-    //             console.log(i.name)
-    //         }
-    //     }
-    // }
+  // const renderGames = () => {
+  //     for(let i in gamesList) {
+  //         console.log(i)
+  //         if(i.name == undefined) {
+  //             console.log(i.name)
+  //         }
+  //     }
+  // }
 
-    // const [letsGoNavigator, setLetsGoNavigator] = useState(false)
+  // const [letsGoNavigator, setLetsGoNavigator] = useState(false)
 
-    // const changeView = (game) => {
-    //     // console.log(game.id, game.name)
-    //     navigation.navigate("Selected game", {game})
-    // }
+  // const changeView = (game) => {
+  //     // console.log(game.id, game.name)
+  //     navigation.navigate("Selected game", {game})
+  // }
 
-    // const navigatorPlz = () => {
-    //     setLetsGoNavigator(!letsGoNavigator)
-    // }
+  // const navigatorPlz = () => {
+  //     setLetsGoNavigator(!letsGoNavigator)
+  // }
 
-    let contWidth = 300
-    let contHeight = 58
-
-    return (
-        <View style={[styles.container, {
-            flexDirection: "column",
-            alignItems: 'center'
-          }]}>
-            <Card style={{width: contWidth, height: contHeight*gamesList.games.length, borderRadius: 20}}>
+  return (
+    <View style={[styles.container, {
+      flexDirection: "column",
+      alignItems: 'center'
+    }]}>
+      <Card style={[styles.fixedGamesDisplay]}>
         {gamesList.games.map(game => {
-            return (
-                
-                <View style={{paddingLeft:contWidth/3, width:200}}key={game.id}>
+          return (
 
-                <br></br>
-                <Button onPress={() => navigation.navigate("Selected game", {game})} title={game.name}/>
-                </View>
-                
-                )
-              })}
-              </Card>
-            {/* <button onClick={() => navigatorPlz()}>navigate</button> */}
-        
-        {/* {letsGoNavigator && navigation.navigate("House rules")} */}
-        </View>
-    )
+            <View style={[styles.fixedSingularGame]} key={game.id}>
+              <Button onPress={() => navigation.navigate("Selected game", { game })} title={game.name} />
+            </View>
+
+          )
+        })}
+      </Card>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  gamesDisplay: {
+    borderRadius: 20,
+    width: wp("90%"),
+    height: hp("10%") * gamesList.games.length
+  },
+  singleGameDisplay: {
+    paddingTop: hp("2.5%"),
+    paddingLeft: wp("90%")/3,
+    width: wp("60%")
+  },
+  fixedSingularGame:{
+    padding: 5,
+    width: 300
+  },
+  fixedGamesDisplay: {
+    borderRadius: 20,
+    paddingTop: 20,
+    width: 300,
+    height: 58 * gamesList.games.length
+  }
+});
 
 export default Games
