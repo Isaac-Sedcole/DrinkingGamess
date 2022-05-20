10e342322c1e592427e9a472bd420b0dbe941308
Isaac Sedcole <isaacsedcolei@gmail.com>
2022-05-21 11:51:48 +1200

    added punishment wheel data

---
+++ data/punishmentWheel.js
@@ -0,0 +1,64 @@
+export default {
+  punishmentWheel: [
+      {
+          id: 1,
+          name: "Take a shot",
+          description: "Grab a shot glass and down it"
+      },
+      {
+        id: 1,
+        name: "Make a wizard staff",
+        description: "Duct tape cans together to form a staff - if you already have one, knight someone"
+      },
+      {
+        id: 1,
+        name: "Truth",
+        description: "Some secrets will be spilled"
+      },
+      {
+        id: 1,
+        name: "Dare",
+        description: "Ready to get fucked?"
+      },
+      {
+        id: 1,
+        name: "Take a sip",
+        description: "Take a sip from your beverage"
+      },
+      {
+        id: 1,
+        name: "No socks for rest of night",
+        description: "Take your socks off"
+      },
+      {
+        id: 1,
+        name: "Nothing you lucked out",
+        description: "Hit it again, you won't"
+      },
+      {
+        id: 1,
+        name: "All for one",
+        description: "Everyone pours a little bit of their drink into yours"
+      },
+      {
+        id: 1,
+        name: "Funnel",
+        description: "Grab a funnel and suck it down"
+      },
+      {
+        id: 1,
+        name: "Helmet",
+        description: "Put something on your head"
+      },
+      {
+        id: 1,
+        name: "Paralysis",
+        description: "You can't use your arms for ANYTHING until the end of the game."
+      },
+      {
+        id: 1,
+        name: "*Physical abuse*",
+        description: "*To be updated"
+      },
+  ]
+}
