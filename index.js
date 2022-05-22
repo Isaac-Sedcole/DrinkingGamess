126fabff1aaff418781634612125c05faa46818c
Isaac Sedcole <isaacsedcolei@gmail.com>
2022-05-22 12:59:21 +1200

    fixed gradle issues that were stopping the app from being built

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom'
 import { Provider } from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
+import { registerRootComponent } from 'expo';
 
 import reducers from './reducers'
 import App from './components/App'
@@ -12,11 +13,14 @@ const store = createStore(reducers, composeEnhancers(
   applyMiddleware(thunk)
 ))
 
+
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Provider store={store}>
       <App />
     </Provider>,
     document.getElementById('app')
-  )
-})
+    )
+  })
+  
+  registerRootComponent(App)
