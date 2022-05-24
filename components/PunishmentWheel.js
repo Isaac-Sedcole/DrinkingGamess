c0c3eaa477e4f7428cf0422403b07853004cb021
Isaac Sedcole <isaacsedcolei@gmail.com>
2022-05-24 13:05:53 +1200

    added some code/pointers for the future of punishment

--- components/PunishmentWheel.js
+++ components/PunishmentWheel.js
@@ -63,3 +63,121 @@ const styles = StyleSheet.create({
   },
 });
 export default PunishmentWheel
+
+// https://www.npmjs.com/package/react-native-reanimated-carousel
+
+// CSGO case slide code (JS)
+// var cardList 	= $('.cardList').first(), 
+//     cards 		= $('.card'), 
+//     speed 		= 1000,
+//     width 		= 100, 
+//     randomize 	= true, 
+//     distance 	= 20 * width 
+// ;
+
+// for (var i = 0; i < 50; i++) {
+//     cards.clone().appendTo(cardList);
+// }
+
+// function spin() {
+//     var newMargin = 0, newDistance = distance;
+//     if (randomize) {
+//         newDistance = Math.floor(Math.random() * cards.length * 5);
+// 		    newDistance += cards.length * 10;
+//         var rand = Math.floor((Math.random()*100)+1);
+//         newDistance *= rand;
+//     } 
+// 	newMargin = -(newDistance);
+//     cards.first().animate({
+//         marginLeft: newMargin
+//     }, 7500);
+// }
+
+// $('#spin').click(function() {
+//     //cards.first().css('margin-left', 0);
+//     setTimeout(spin,500);
+//     return false;
+// });
+
+// CSGO case slide code (CSS)
+// * {
+//   box-sizing: border-box;
+//   padding: 0;
+//   margin: 0;
+// }
+
+// .cardList {
+//   height: 100px;
+//   width: 302px;
+//   position: relative;
+//   margin: 10px;
+//   border: 1px solid #33e;
+//   overflow: hidden;
+//   white-space: nowrap;
+// }
+
+// .card {
+//   display: inline-block;
+//   text-align: center;
+//   height: 100px;
+//   width: 100px;
+//   line-height: 100px;
+//   background-color: #99e;
+//   font-family: monospace;
+//   font-size: 2em;
+//   color: #444;
+//   border-left: 1px solid #33e;
+//   border-right: 1px solid #33e;
+// }
+
+// .cardList::before,
+// .cardList::after {
+//   content: '';
+//   display: block;
+//   z-index: 100;
+//   width: 0px;
+//   height: 0px;
+//   transform: translateX(-50%);
+//   border-left: 8px solid transparent;
+//   border-right: 8px solid transparent;
+// }
+
+// .cardList::before {
+//   position: absolute;
+//   top: 0px;
+//   left: 50%;
+//   border-top: 12px solid #33e;
+// }
+
+// .cardList::after {
+//   position: absolute;
+//   bottom: 0px;
+//   left: 50%;
+//   border-bottom: 12px solid #33e;
+// }
+
+// .common {
+//   background-color: green;
+//   width: 200px;
+// }
+// .rare {
+//   background-color: lightblue;
+//   width: 100px;
+// }
+// .mythical {
+//   background-color: yellow;
+//   width: 50px;
+// }
+// .legendary {
+//   background-color: orange;
+//   width: 10px;
+// }
+
+// CSGO case opening code (HTML)
+{/* <div class="cardList"><!--
+    --><div class="card common">Common</div><!--
+    --><div class="card rare">Rare</div><!--
+    --><div class="card mythical">M</div><!--
+    --><div class="card legendary">L</div><!--
+--></div>
+<button id="spin">Spin</button> */}
