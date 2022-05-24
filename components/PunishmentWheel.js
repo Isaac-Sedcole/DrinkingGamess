fd7e39e956c719e5c45174ca879257ae34ddd7df
Isaac Sedcole <isaacsedcolei@gmail.com>
2022-05-24 12:07:58 +1200

    added punishments and randomiser - needs styling'

--- components/PunishmentWheel.js
+++ components/PunishmentWheel.js
@@ -1,18 +1,51 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, TouchableHighlight, View, Button } from 'react-native'
 //import  WheelOfFortune  from 'react-native-wheel-of-fortune'
 // import knob from '../assets/images/knob.png'
 //mport knob from './ImageManager'//
 
 import punishmentWheel from '../data/punishmentWheel'
+import AppButton from './AppButton'
 
-function PunishmentWheel() {
+function PunishmentWheel(props) {
   const punishWheel = punishmentWheel.punishmentWheel
+
+  const [showPunishment, setShowPunishment] = useState(false)
+  const [punishment, setPunishment] = useState(null)
+
+  const[goToRuleModal, setGoToRuleModal] = useState(false)
+
+  useEffect(()=>{
+    setShowPunishment(false)
+    setPunishment(null)
+    setGoToRuleModal(false)
+  }, [])
+
+  const selectRandomPunishment = () => {
+    console.log(punishWheel)
+    let randPunishment = Math.floor(Math.random() * punishWheel.length)
+    // console.log(randPunishment, punishWheel[randPunishment])
+    setPunishment(punishWheel[randPunishment])
+    setShowPunishment(true)
+    setGoToRuleModal(false)
+  }
+
+  const navigateToRuleModal = () => {
+    setGoToRuleModal(true)
+  }
 
 
   return(
     <View>
-      <Text>Hi </Text> 
+      <View>
+        <AppButton onPress={selectRandomPunishment} title='Get Punished'/>
+      </View>
+      {showPunishment && 
+      <View>
+        <AppButton onPress={navigateToRuleModal} title={punishment.name}/>
+      </View>
+      }
+      {goToRuleModal && props.navigation.navigate("Showing a house rule", { rule: punishment })}
     </View>
   )
 
