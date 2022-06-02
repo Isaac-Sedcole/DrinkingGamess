b7bf25cf7457def66d7ad84fa681da5aa837cad8
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2022-06-03 10:54:31 +1200

    Pondering if booze or no booze requires a specific page

--- App.js
+++ App.js
@@ -63,7 +63,7 @@ function App() {
           <Stack.Screen name="Kings Cup" component={KingsCup} />
           <Stack.Screen name="Beer Pong" component={BeerPong} />
           <Stack.Screen name="Cheers Governor" component={CheersGovernor} />
-          <Stack.Screen name="Booze or no Booze" component={BoozeOrNoBooze} />
+          <Stack.Screen name="Booze or no Booze" component={BoozeOrNoBooze} /> //I wonder if this is necessary or if it could be routed back to the original game component
           <Stack.Screen name="Play Cheers Governor" component={PlayCheersGovernor} />
         </Stack.Navigator>
       </NavigationContainer>
