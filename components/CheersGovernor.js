86408a7d7d4d538f6b2dc84683b35f97ae6c11db
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2022-06-03 11:03:32 +1200

    Updated how text appears so that it shows full rule description

--- components/CheersGovernor.js
+++ components/CheersGovernor.js
@@ -86,7 +86,7 @@ function CheersGovernor(props) {
                   <View style={[styles.cardContainer]}>
                     <View style={[styles.cardContent]}>
                       <AppButton onPress={activateShowRuleModal} title='Show Rules' />
-                      {showRuleModal && <Text>{ruleDescObj}</Text>}
+                      {showRuleModal && <Text style={[styles.rulesText]}>{ruleDescObj}</Text>}
                     </View>
                   </View>
                 </View>
@@ -105,6 +105,7 @@ function CheersGovernor(props) {
               {showRuleModal && <Text style={[styles.descriptionText]}>{currentRule.desc}</Text>}
             </View> */}
           </View>
+          {/** {showRuleModal && <Text style={[styles.rulesText]}>{ruleDescObj}</Text>} */}
         </View>
       </ScrollView>
     </>
