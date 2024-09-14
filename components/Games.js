c15e582822529a7bd0c7b156687a5f18ef17b4c8
Isaac Sedcole <isaacsedcolei@gmail.com>
2024-09-14 14:20:51 +1200

    Updated things to let game be built by expo

--- components/Games.js
+++ components/Games.js
@@ -1,21 +1,45 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import { Card } from 'react-native-paper'
-import gamesList from '../data/gamesList'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-paper';
+import gamesList from '../data/gamesList';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-import Game from './Game'
-import HouseRules from './HouseRules'
+import Game from './Game';
+import HouseRules from './HouseRules';
 
 function Games({ navigation }) {
 
   const navigateToGame = (game) => {
     if (game.customRules.length > 0) {
-      navigation.navigate(game.name, { game })
+      navigation.navigate(game.name, { game });
     } else {
-      navigation.navigate("Selected game", { game })
+      navigation.navigate("Selected game", { game });
     }
-  }
+  };
+
+  const getIcon = (iconName) => {
+    switch (iconName) {
+      case 'KingsCup':
+        return require('../assets/GameIcons/KingsCup.png');
+      case 'SmashCup':
+        return require('../assets/GameIcons/SmashCup.png');
+      case 'TaxiDriver':
+        return require('../assets/GameIcons/TaxiDriver.png');
+      case 'Fingers':
+        return require('../assets/GameIcons/Fingers.png');
+      case 'Horses':
+        return require('../assets/GameIcons/Horses.png');
+      case 'BeerPong':
+        return require('../assets/GameIcons/BeerPong.png');
+      case 'CheersGov':
+        return require('../assets/GameIcons/CheersGov.png');
+      case 'BoozeOrNoBooze':
+        return require('../assets/GameIcons/BoozeOrNoBooze.png');
+      // Add more cases as needed
+      default:
+        return require('../assets/knob.png'); // Fallback icon
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -29,17 +53,17 @@ function Games({ navigation }) {
             >
               <Card style={styles.card}>
                 <Image 
-                  source={require(`../assets/gameicons/${game.iconName}.png`)} 
+                  source={getIcon(game.iconName)} 
                   style={styles.gameIcon} 
                 />
                 <Text style={styles.gameName}>{game.name}</Text>
               </Card>
             </TouchableOpacity>
-          )
+          );
         })}
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -74,4 +98,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Games
+export default Games;
