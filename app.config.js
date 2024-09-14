232664b63754b33c31bad60f252df613084bbd36
Isaac Sedcole <isaacsedcolei@gmail.com>
2024-09-14 16:59:57 +1200

    needs this to build correctly

--- app.config.js
+++ app.config.js
@@ -1,24 +1,43 @@
 export default {
   expo: {
-    plugins: [
-      [
-        'expo-build-properties',
-        {
-          android: {
-            compileSdkVersion: 34,
-            targetSdkVersion: 34,
-            buildToolsVersion: '34.0.0',
-          },
-          ios: {
-            deploymentTarget: '13.4',
-          },
-        },
-      ],
+    name: "drinkinggamess",
+    slug: "drinkinggamess",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/My-splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: [
+      "**/*"
     ],
-    "extra": {
-      "eas": {
-        "projectId": "c46f7a0e-feab-4f01-8ead-0ea788daf600"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.drsaac.drinkinggamess"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/My-adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+      package: "com.drsaac.drinkinggamess" // Ensure this line is present
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    description: "",
+    extra: {
+      eas: {
+        projectId: "c46f7a0e-feab-4f01-8ead-0ea788daf600"
       }
-    }
-  },
+    },
+    plugins: [
+      "expo-build-properties",
+    ]
+  }
 };
